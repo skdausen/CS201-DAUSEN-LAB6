be98b560b652eb78523455cf7939c4751aed5192
Sean Kelly Dausen <146722016+skdausen@users.noreply.github.com>
2023-10-10 10:14:10 +0800

    feat: Activity 6 in DSA
    while loop in JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+//Activity 6
+
+//print your name 100 times to the console using while loop
+//iteration will start from 10
+//code with the while loop
+
+let i = 10;
+
+while (i <= 110){
+    console.log('Sean Kelly Dausen', i);
+    i++;
+}
